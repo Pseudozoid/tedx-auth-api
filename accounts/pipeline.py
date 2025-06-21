44c2ba1a0993e6704506ed393c106cd23d8232b7
Pseudozoid <sarathmadhavr@gmail.com>
2025-06-21 12:05:35 +0530

    fix OAuth pipeline

--- accounts/pipeline.py
+++ accounts/pipeline.py
@@ -1,10 +1,9 @@
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import HttpResponseRedirect
 
 def generate_jwt_for_user(backend, user, request, *args, **kwargs):
-    if user and request:
+    if user:
         refresh = RefreshToken.for_user(user)
-        request.session['jwt_token'] = {
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        }
+        access = str(refresh.access_token)
+        return HttpResponseRedirect(f"https://tedx-auth-frontend.onrender.com?access={access}")
 
